1d3c4b563b53aab7e9c59974a0f99462ef04b8ee
Chenbo Li <lichenbo1949@gmail.com>
2013-03-19 19:02:25 +0800

    update url and io

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import xml.etree.ElementTree as ET
 import urllib2
 import os
 import errno
+import sys
 
+if len(sys.argv) > 1 and sys.argv[1] == '-clear':
+	try:
+		os.remove('config.dat')
+	except OSError as exception:
+		print "config file removed." 
+try:	
+	local = open('config.dat','r+')
+except IOError as exception:
+	local = open('config.dat','w+')
 
-local = open('config.dat','w+')
 dest = local.read()
 if dest=="":
 		dest = raw_input("Please enter the directory:")
@@ -17,14 +26,16 @@ if dest=="":
 		if dest[-1] != '/' and dest[-1] != '\\':
 				dest+='/'
 		local.write(dest)
-tree = ET.parse(urllib2.urlopen('http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US'))
-root = tree.getroot()
-user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.160 Safari/537.22'
-for item in root.iter('item'):
-		dlink = str(item.find('link').get('ref'))
-		dname = str(item.find('title').text)
-		print ('Downloading ' + dlink + ' to ' + dest)
-		pic = urllib2.urlopen(urllib2.Request(dlink.replace(" ","%20"),None,{'User-Agent' : user_agent}))
-		output = open(dest+dname+'.jpg','wb+')
-		output.write(pic.read())
-		output.close()
+local.close()
+
+for i in range(10):
+	tree = ET.parse(urllib2.urlopen('http://www.bing.com/HPImageArchive.aspx?format=xml&idx='+str(i)+'&n=1&mkt=en-US'))
+	root = tree.getroot()
+	user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.160 Safari/537.22'
+	dlink = str('http://www.bing.com'+root.find('image').find('url').text)
+	dname = str(root.find('image').find('startdate').text)
+	print ('Downloading ' + dname + ' to ' + dest)
+	pic = urllib2.urlopen(urllib2.Request(dlink.replace(" ","%20"),None,{'User-Agent' : user_agent}))
+	output = open(dest+dname+'.jpg','wb+')
+	output.write(pic.read())
+	output.close()
